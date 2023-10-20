eb7effb8ef1da6e8ffd37cb323e93f1eb2208d52
Simply92 <pedrouner92@gmail.com>
2023-10-20 14:57:50 -0300

    listo

--- src/controllers/postFav.js
+++ src/controllers/postFav.js
@@ -1,55 +1,25 @@
-const { Favorite } = require("../DB_connection"); //por qué se importa desde db en vez de desde models?
+const {Favorite} = require('../DB_connection');
 
-const postFav = async (req, res) => {
-  try {
-    const { id, name, status, origin, image, species, gender } = req.body;
-    if (!id || !name || !status || !image || !species || !gender) {
-      return res.status(401).send("Faltan datos");
-    }
-    await Favorite.findOrCreate({
-      where: {
-        id,
-        name,
-        origin,
-        status,
-        image,
-        species,
-        gender,
-      },
-    });
-    const favorites = await Favorite.findAll();
-    res.status(200).json(favorites);
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
-  }
-};
-
-module.exports = postFav;
-
-
-
-// const {Favorite} = require('../DB_connection');
-
-// const postFav = async(req, res) => {
-//    try {
-//     const {id, name, origin, status, image, species, gender} = req.body;
-//     if(!id || !name || !origin || !status || !image || !species || !gender){
-//         return res.status(401).send('missing data')
-//        } 
-//        await Favorite.findOrCreate({
-//         where: {
-//             id, name, origin, status, image, species, gender
-//         }
-//        })
-//        const allFav = await Favorite.findAll()
-//        return res.status(200).json(allFav)
+const postFav = async(req, res) => {
+   try {
+    const {id, name, origin, status, image, species, gender} = req.body;
+    if(!id || !name || !origin || !status || !image || !species || !gender){
+        return res.status(401).send('missing data')
+       } 
+       await Favorite.findOrCreate({
+        where: {
+            id, name, origin, status, image, species, gender
+        }
+       })
+       const allFav = await Favorite.findAll()
+       return res.status(200).json(allFav)
     
-//    } catch (error) {
-//     return res.status(500).json({ error: error.message });
-//    }
-// }
+   } catch (error) {
+    return res.status(500).json({ error: error.message });
+   }
+}
 
-// module.exports= postFav;
+module.exports= postFav;
 
 
 
